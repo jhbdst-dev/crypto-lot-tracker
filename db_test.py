75c260e29006e8b02bc3195d90cd05169323c380
jhbdst-dev <jhs.bdst@gmail.com>
2026-06-05 15:03:57 +0900

    DB 데이터 불러오기 테스트 완료
    - db_test.py 실행으로 trades의 데이터 불러오기

--- db_test.py
+++ db_test.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# DB 연결
 conn = psycopg2.connect(
     dbname="crypto_lot_tracker",
     user="hs",
@@ -7,14 +8,19 @@ conn = psycopg2.connect(
     port="5432"
 )
 
+# SQL 실행 준비
 cur = conn.cursor()
 
+# SQL 실행
 cur.execute("SELECT * FROM trades")
 
+# 결과 가져오기
 rows = cur.fetchall()
 
+# 출력
 for row in rows:
     print(row)
 
+# 정리
 cur.close()
 conn.close()
