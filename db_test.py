e93adb47e89ecb6814cf1141bd6c90bd982defe0
jhbdst-dev <jhs.bdst@gmail.com>
2026-06-08 17:25:41 +0900

    Update db_test.py
    주석으로 살짝 정리

--- db_test.py
+++ db_test.py
@@ -17,7 +17,10 @@ cur.execute("SELECT * FROM trades")
 # 결과 가져오기
 rows = cur.fetchall()
 
+# 총 보유수량
 total_quantity = 0
+
+# 총 매수금액
 total_buy_amount = 0
 
 # 출력
@@ -27,6 +30,8 @@ for row in rows:
 
 print(f"총 보유수량: {total_quantity}")
 print(f"총 매수금액: {total_buy_amount}")
+
+# 평균 매수가
 average_buy_price = total_buy_amount / total_quantity
 print(f"평균 매수가: {average_buy_price}")
 
