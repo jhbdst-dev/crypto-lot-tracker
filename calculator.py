0929f2ced2f8e59b20865531c91b8b799161fb20
jhbdst-dev <jhs921020@nate.com>
2026-06-22 06:13:58 +0900

    calculate_asset()에서 print 제거

--- calculator.py
+++ calculator.py
@@ -21,26 +21,7 @@ def calculate_asset(rows, current_price, fee_rate):
         transaction_profit_loss = value - settlement_amount
         transaction_profit_rate = (transaction_profit_loss / settlement_amount) * 100
 
-        #3. 거래별 손익 출력
-        print("\n" + "=" * 50)
-        print(f"{row[0]} 번째 코인")
-        print(f"매수가: {price:,.0f}원")
-        print(f"수량: {quantity:,.8f}개")
-        print(f"거래금액: {trade_amount:,.0f}원")
-        print(f"수수료: {fee_amount:,.3f}원")
-        print(f"정산금액: {settlement_amount:,.0f}원")
-        print(f"평가금액: {value:,.0f}원")
-        print(f"평가손익: {transaction_profit_loss:,.0f}원")
-        print(f"수익률: {transaction_profit_rate:,.2f}%")
-        
-        if transaction_profit_loss > 0:
-            print("이익 🟢")
-        else:
-            print("손해 🔴")
-
-        print("=" * 50)
-
-        # 4. 전체 합계 누적
+        # 3. 전체 합계 누적
         total_quantity += quantity
         total_buy_amount += settlement_amount
 
@@ -55,9 +36,6 @@ def calculate_asset(rows, current_price, fee_rate):
 
     # 수익률
     profit_rate = (profit_loss / total_buy_amount) * 100
-
-    print(f"총 보유수량: {total_quantity:,.8f}개")
-    print(f"총 매수금액: {total_buy_amount:,.0f}원")
 
     return (
         total_quantity,
