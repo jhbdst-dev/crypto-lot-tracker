af0d846beeec9e7d07a8496fd10c85ad9f5140b4
jhbdst-dev <jhs921020@nate.com>
2026-07-09 20:33:32 +0900

    보유자산 출력

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from upbit_ws import watch_current_price
 # 출력
 from printer import print_per_trade_results
 from printer import print_current_price
+from printer import print_asset_summary
 
 # 거래내역 데이터 저장
 rows = get_trades()
@@ -56,6 +57,16 @@ print_per_trade_results(per_trade_results)
 # 현재가 출력
 print_current_price(current_price)
 
+# 보유자산 출력
+print_asset_summary(
+    total_quantity,
+    total_buy_amount,
+    average_buy_price,
+    current_value,
+    profit_loss,
+    profit_rate
+)
+
 # 매도 결과
 print("\n" + "=" * 50)
 print("             매도 결과")
@@ -73,15 +84,3 @@ for sell in sell_trade_results:
 
 print("=" * 50)
 
-print("\n" + "=" * 50)
-print("             보유자산 화면")
-print("=" * 50)
-
-print(f"총 보유수량: {total_quantity:.8f}")
-print(f"총 매수금액: {total_buy_amount:,.0f}원")
-print(f"평균 매수가: {average_buy_price:,.2f}원")
-print(f"현재 평가금액: {current_value:,.0f}원")
-print(f"평가손익: {profit_loss:,.0f}원")
-print(f"수익률: {profit_rate:.2f}%")
-
-print("=" * 50)
--- printer.py
+++ printer.py
@@ -19,11 +19,31 @@ def print_per_trade_results(per_trade_results):
         else:
             print("손해 🔴")
 
-
-
 # 현재가 출력
 def print_current_price(current_price):
     print("\n" + "=" * 50)
     print("             현재가")
     print("=" * 50)
     print(f"현재가: {current_price}")
+
+# 보유자산 출력
+def print_asset_summary(
+    total_quantity,
+    total_buy_amount,
+    average_buy_price,
+    current_value,
+    profit_loss,
+    profit_rate
+):
+    print("\n" + "=" * 50)
+    print("             보유자산 화면")
+    print("=" * 50)
+
+    print(f"총 보유수량: {total_quantity:.8f}")
+    print(f"총 매수금액: {total_buy_amount:,.0f}원")
+    print(f"평균 매수가: {average_buy_price:,.2f}원")
+    print(f"현재 평가금액: {current_value:,.0f}원")
+    print(f"평가손익: {profit_loss:,.0f}원")
+    print(f"수익률: {profit_rate:.2f}%")
+
+    print("=" * 50)
